f5af052f7f6c5c5ed5882d6284b7b97739cce486
FranciscoAguero247 <154863495+FranciscoAguero247@users.noreply.github.com>
2025-05-11 02:03:37 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_KEY = "08320c34b5841d4c7f44b0de6bd6a635";
+
+/**
+ * retrive and convert zip code to lat and lon to get weather forecast
+ */
+
+app.use('/public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended : true }));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) =>{
+    res.render("index.ejs");
+})
+
+app.post("/submit", async (req, res) => {
+    
+    const zipCode = req.body["zipcode"];
+    const resultGeocoding = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${API_KEY}`);
+
+    const latitude = JSON.stringify(resultGeocoding.data.lat);
+    const longtitude = JSON.stringify(resultGeocoding.data.lon);
+
+    const resultWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`);
+    
+    const iconCode = JSON.stringify(resultWeather.data.weather[0].icon);
+    const cityNameStr = JSON.stringify(resultWeather.data.name);
+    const cityName = cityNameStr.replace(/['"]+/g, '');
+    /**
+     * turn icon code to image in html page
+     */
+
+    const temperature = JSON.stringify(resultWeather.data.main.temp);
+    const convretedTemperature = Math.round(((temperature - 273.15) * (9/5)) + 32);
+    /**
+     * add five day weather forecast to html page underneath weather block
+     */
+    const resultForecast = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`);
+    let weatherList = resultForecast.data.list;
+
+    let days =[];
+    let tempList = [];
+    let iconList = [];
+    for (let i = 0; i < weatherList.length; i += 8) {
+        const tempWeekForecast = Math.round((((weatherList[i].main.temp) - 273.15) * (9/5)) + 32);
+        const weekForecastIcon = weatherList[i].weather[0].icon;
+        let dayname = new Date(weatherList[i].dt * 1000).toLocaleDateString("en", {weekday: "long",});
+        days.push(dayname);
+        tempList.push(tempWeekForecast);
+        iconList.push(weekForecastIcon);
+    }
+    
+
+
+    
+    res.render("index.ejs", {
+        city : cityName, 
+        temperature : convretedTemperature,
+        icon : iconCode,
+        weekIcon : iconList,
+        day: days,
+        temperatureWeekForecast : tempList
+        });
+
+    // }catch(error){
+    //     weather = null;
+    //     error = "Error, Please try again";
+    //     res.render("index.ejs", {weather, error});
+    // }//add ejs error variable to html page
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
