addede07cc8daa154c9ac24a15ad04dd2b5564b6
FranciscoAguero247 <blast255@live.com>
2025-07-12 21:37:22 -0600

    fixed the 500 internal error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-const port = 3000;
+const port = 3001;
 const api_key = process.env.API_KEY;
 
 
@@ -81,6 +81,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views/page500.html'));
 });
+
+export default app;
+app.listen(process.env.port || 2121);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
